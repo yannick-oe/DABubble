8678b74e7da905098708728857d26a0009109c8e
Yannick Oe <yannickoetelshoven@gmail.com>
2026-06-11 09:07:57 +0200

    feat(auth): changed intro animation, login page and added Firebase bootstrap

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,21 @@
+/**
+ * @file Application-wide provider configuration, including router and Firebase.
+ */
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
